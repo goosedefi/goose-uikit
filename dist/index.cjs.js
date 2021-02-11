437929569230038ece959b8406ae7f6ed74d9f7f
GooseAdmin <admin@goosedefi.com>
2021-02-12 01:30:51 +0800

    update release

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2126,6 +2126,50 @@ var socials = [
                 label: "English",
                 href: "https://t.me/goosedefi",
             },
+            // {
+            //   label: "Bahasa Indonesia",
+            //   href: "https://t.me/PancakeSwapIndonesia",
+            // },
+            // {
+            //   label: "中文",
+            //   href: "https://t.me/PancakeSwap_CN",
+            // },
+            // {
+            //   label: "Tiếng Việt",
+            //   href: "https://t.me/PancakeSwapVN",
+            // },
+            // {
+            //   label: "Italiano",
+            //   href: "https://t.me/pancakeswap_ita",
+            // },
+            // {
+            //   label: "русский",
+            //   href: "https://t.me/pancakeswap_ru",
+            // },
+            // {
+            //   label: "Türkiye",
+            //   href: "https://t.me/pancakeswapturkiye",
+            // },
+            // {
+            //   label: "Português",
+            //   href: "https://t.me/PancakeSwapPortuguese",
+            // },
+            // {
+            //   label: "Español",
+            //   href: "https://t.me/PancakeswapEs",
+            // },
+            // {
+            //   label: "日本語",
+            //   href: "https://t.me/pancakeswapjp",
+            // },
+            // {
+            //   label: "Français",
+            //   href: "https://t.me/pancakeswapfr",
+            // },
+            {
+                label: "Announcements",
+                href: "https://t.me/goosefinanceann",
+            },
         ],
     },
     {
