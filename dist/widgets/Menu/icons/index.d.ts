cc2396cb18e3a1dba6a178ebbc4cb18bfd005331
GooseAdmin <admin@goosedefi.com>
2021-02-22 00:29:15 +0800

    add audit icon

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -16,3 +16,4 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as AuditIcon } from "./Audit";
