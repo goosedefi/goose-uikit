f939b18fbf5e5c36f0011f96779c60243bc7703e
GooseAdmin <admin@goosedefi.com>
2021-02-27 23:09:41 +0800

    add goose icon

--- src/components/Svg/index.tsx
+++ src/components/Svg/index.tsx
@@ -41,5 +41,6 @@ export { default as SearchIcon } from "./Icons/Search";
 export { default as SwapVertIcon } from "./Icons/SwapVert";
 export { default as SyncAltIcon } from "./Icons/SyncAlt";
 export { default as WarningIcon } from "./Icons/Warning";
+export { default as GooseIcon } from "./Icons/Goose";
 export { default as Svg } from "./Svg";
 export type { SvgProps } from "./types";
