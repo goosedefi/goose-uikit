df1591aaaff003afb1bb80d4efb3824e53f80b47
GooseAdmin <admin@goosedefi.com>
2021-03-07 06:05:25 +0800

    update telegram groups

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2148,10 +2148,10 @@ var socials = [
             //   label: "Bahasa Indonesia",
             //   href: "https://t.me/PancakeSwapIndonesia",
             // },
-            // {
-            //   label: "中文",
-            //   href: "https://t.me/PancakeSwap_CN",
-            // },
+            {
+                label: "中文",
+                href: "https://t.me/goosefinancechinese",
+            },
             // {
             //   label: "Tiếng Việt",
             //   href: "https://t.me/PancakeSwapVN",
@@ -2160,10 +2160,10 @@ var socials = [
             //   label: "Italiano",
             //   href: "https://t.me/pancakeswap_ita",
             // },
-            // {
-            //   label: "русский",
-            //   href: "https://t.me/pancakeswap_ru",
-            // },
+            {
+                label: "русский",
+                href: "https://t.me/goosefinancerussian",
+            },
             // {
             //   label: "Türkiye",
             //   href: "https://t.me/pancakeswapturkiye",
@@ -2172,14 +2172,14 @@ var socials = [
             //   label: "Português",
             //   href: "https://t.me/PancakeSwapPortuguese",
             // },
-            // {
-            //   label: "Español",
-            //   href: "https://t.me/PancakeswapEs",
-            // },
-            // {
-            //   label: "日本語",
-            //   href: "https://t.me/pancakeswapjp",
-            // },
+            {
+                label: "Español",
+                href: "https://t.me/goosefinancespanish",
+            },
+            {
+                label: "日本語",
+                href: "https://t.me/goosefinancejapanese",
+            },
             // {
             //   label: "Français",
             //   href: "https://t.me/pancakeswapfr",
@@ -2187,6 +2187,10 @@ var socials = [
             {
                 label: "Announcements",
                 href: "https://t.me/goosefinanceann",
+            },
+            {
+                label: "Price Bot",
+                href: "https://t.me/BinanceRocketEGG",
             },
         ],
     },
--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -126,10 +126,10 @@ export const socials = [
       //   label: "Bahasa Indonesia",
       //   href: "https://t.me/PancakeSwapIndonesia",
       // },
-      // {
-      //   label: "中文",
-      //   href: "https://t.me/PancakeSwap_CN",
-      // },
+      {
+        label: "中文",
+        href: "https://t.me/goosefinancechinese",
+      },
       // {
       //   label: "Tiếng Việt",
       //   href: "https://t.me/PancakeSwapVN",
@@ -138,10 +138,10 @@ export const socials = [
       //   label: "Italiano",
       //   href: "https://t.me/pancakeswap_ita",
       // },
-      // {
-      //   label: "русский",
-      //   href: "https://t.me/pancakeswap_ru",
-      // },
+      {
+        label: "русский",
+        href: "https://t.me/goosefinancerussian",
+      },
       // {
       //   label: "Türkiye",
       //   href: "https://t.me/pancakeswapturkiye",
@@ -150,14 +150,14 @@ export const socials = [
       //   label: "Português",
       //   href: "https://t.me/PancakeSwapPortuguese",
       // },
-      // {
-      //   label: "Español",
-      //   href: "https://t.me/PancakeswapEs",
-      // },
-      // {
-      //   label: "日本語",
-      //   href: "https://t.me/pancakeswapjp",
-      // },
+      {
+        label: "Español",
+        href: "https://t.me/goosefinancespanish",
+      },
+      {
+        label: "日本語",
+        href: "https://t.me/goosefinancejapanese",
+      },
       // {
       //   label: "Français",
       //   href: "https://t.me/pancakeswapfr",
@@ -166,10 +166,10 @@ export const socials = [
         label: "Announcements",
         href: "https://t.me/goosefinanceann",
       },
-      // {
-      //   label: "Whale Alert",
-      //   href: "https://t.me/PancakeSwapWhales",
-      // },
+      {
+        label: "Price Bot",
+        href: "https://t.me/BinanceRocketEGG",
+      },
     ],
   },
   {
