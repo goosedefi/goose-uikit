d059a9d1a194bd415bf01637a8270d0161eaba2c
GooseAdmin <admin@goosedefi.com>
2021-03-26 05:32:16 +0800

    update socials icon margin

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2360,7 +2360,7 @@ var PanelFooter = function (_a) {
             React__default['default'].createElement(Flex, null, socials.map(function (social, index) {
                 var Icon = Icons$1[social.icon];
                 var iconProps = { width: "24px", color: "textSubtle", style: { cursor: "pointer" } };
-                var mr = index < socials.length - 1 ? "24px" : 0;
+                var mr = index < socials.length - 1 ? "12px" : 0;
                 if (social.items) {
                     return (React__default['default'].createElement(Dropdown, { key: social.label, position: "top", target: React__default['default'].createElement(Icon, __assign({}, iconProps, { mr: mr })) }, social.items.map(function (item) { return (React__default['default'].createElement(Link, { external: true, key: item.label, href: item.href, "aria-label": item.label, color: "textSubtle" }, item.label)); })));
                 }
